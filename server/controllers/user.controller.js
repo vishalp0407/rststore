d2a23119495c266d8a995b3279326f7dcedf14f6
Paul Philips <paulphilips@email.com>
2025-04-25 17:40:09 +0530

    Step 35 completed: - Added logic to generate JWT and send it as a http-only cookie

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 import UserModel from "#models/user.model.js";
 
 /**
@@ -11,6 +13,15 @@ const authUser = async (req, res) => {
   const user = await UserModel.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
     res.json({
       _id: user._id,
       name: user.name,
