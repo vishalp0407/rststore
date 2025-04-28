cddd776ec2f071ed5005c8382f6dcb176ae06570
Paul Philips <paulphilips@email.com>
2025-04-28 14:58:12 +0530

    Step 39 completed: - Created authSlice and usersApiSlice for frontend authentications

---
+++ client/src/slices/userApiSlice.js
@@ -0,0 +1,16 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: `${USERS_URL}/login`,
+        method: "POST",
+        body: credentials,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
